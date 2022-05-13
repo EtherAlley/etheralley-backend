fe014e8f156e7a265674910219093a6b115a6fef
Evan Fleming <evan.gordon.fleming@gmail.com>
2022-05-12 20:34:56 -0700

    adding logs for debugging ip address headers from reverse proxy

--- controllers/http/real_ip.go
+++ controllers/http/real_ip.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 )
 
+var trueClientIP = http.CanonicalHeaderKey("True-Client-IP")
 var xForwardedFor = http.CanonicalHeaderKey("X-Forwarded-For")
 var xRealIP = http.CanonicalHeaderKey("X-Real-IP")
 
@@ -13,6 +14,13 @@ const localhost = "127.0.0.1"
 // See https://github.com/go-chi/chi/blob/master/middleware/realip.go
 func (hc *HttpController) realIP(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		tcip := r.Header.Get(trueClientIP)
+		xRealIP := r.Header.Get(xRealIP)
+		xForwardedFor := r.Header.Get(xForwardedFor)
+		hc.logger.Info(r.Context()).Msgf("tcip: %v", tcip)
+		hc.logger.Info(r.Context()).Msgf("realip: %v", xRealIP)
+		hc.logger.Info(r.Context()).Msgf("xff: %v", xForwardedFor)
+
 		// RemoteAddr is not in a recognizable format on dev without the 3 headers above
 		// This makes things like recording profile views a little more pleasant on local while keeping validation in place
 		if hc.settings.IsDev() {
@@ -20,6 +28,9 @@ func (hc *HttpController) realIP(h http.Handler) http.Handler {
 		} else if rip := getIP(r); rip != "" {
 			r.RemoteAddr = rip
 		}
+
+		hc.logger.Info(r.Context()).Msgf("remoteaddr: %v", r.RemoteAddr)
+
 		h.ServeHTTP(w, r)
 	})
 }
