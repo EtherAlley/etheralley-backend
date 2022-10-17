b87157ab08a97ae206d72d0bc09e7b3394e50161
Evan Fleming <evan.gordon.fleming@gmail.com>
2022-10-16 18:46:35 -0700

    adding http listener to daos module (#49)

--- daos/main.go
+++ daos/main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"math/rand"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/etheralley/etheralley-backend/common"
+	"github.com/etheralley/etheralley-backend/daos/controller"
+	"github.com/etheralley/etheralley-backend/daos/settings"
+	"go.uber.org/dig"
+)
 
 func main() {
-	fmt.Println("hello world")
+	container := dig.New()
+	container.Provide(context.Background)
+	container.Provide(settings.NewAppSettings)
+	container.Provide(common.NewLogger)
+	container.Provide(controller.NewHttpController)
+
+	// seed the random number generator based on app start time
+	rand.Seed(time.Now().UnixNano())
+
+	// start the http controller inside a go routine
+	if err := container.Invoke(startHttpController); err != nil {
+		panic(err)
+	}
+
+	// blocking call in main go routine to await sigterm
+	if err := container.Invoke(awaitSigterm); err != nil {
+		panic(err)
+	}
+}
+
+func startHttpController(ctx context.Context, controller controller.IHttpController) {
+	go func() {
+		err := controller.Start(ctx)
+		panic(err)
+	}()
+}
+
+func awaitSigterm(ctx context.Context, logger common.ILogger) {
+	logger.Info(ctx).Msg("awaiting sigterm")
+
+	cancelChan := make(chan os.Signal, 1)
+	signal.Notify(cancelChan, syscall.SIGTERM, syscall.SIGINT)
+	sig := <-cancelChan
+
+	logger.Info(ctx).Msgf("caught sigterm %v", sig)
 }
